99b8deaf9fe8c49394879da9fe0fc1342983e4a1
Juninhoww2 <alqadianokira@gmail.com>
2022-05-06 00:13:59 -0300

    mobile📱

---
+++ mobile/src/components/Option/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    TouchableOpacity,
+    TouchableOpacityProps,
+    Image,
+    ImageProps,
+    Text,
+  } from 'react-native';
+
+  
+import { styles } from './styles';
+
+export function Option({ title, image, ...rest }: Props) {
+    return (
+        <TouchableOpacity style={styles.container} {...rest}>
+          <Image source={image} style={styles.image} />
+          <Text style={styles.title}>{title}</Text>
+        </TouchableOpacity>
+    );
+}
+
