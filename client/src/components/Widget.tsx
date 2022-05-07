fe1a83f2fe80c18af055211e1e6edcb6e73f9072
Juninhoww2 <alqadianokira@gmail.com>
2022-05-07 13:37:50 -0300

    Add Awesome new features <3

--- client/src/components/Widget.tsx
+++ client/src/components/Widget.tsx
@@ -11,7 +11,7 @@ export function Widget() {
         <WidgetForm />
       </Popover.Panel>
 
-      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+      <Popover.Button className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 rounded-full px-3 h-12 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6" />
 
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
